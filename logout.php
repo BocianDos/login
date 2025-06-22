838e2ebafa746ef55c6bd7160ec544ace7bb91ee
BocianDos <75833916+BocianDos@users.noreply.github.com>
2025-06-22 23:52:02 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,10 @@
 <?php
-// Inicjalizacja sesji
 session_start();
 
-// Usunięcie wszystkich zmiennych sesji
 $_SESSION = array();
 
-// Zniszczenie sesji
 session_destroy();
 
-// Przekierowanie na stronę główną
 header("location: index.php");
 exit;
 ?>
